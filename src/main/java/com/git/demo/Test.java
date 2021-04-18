768c9748df043c71b53f2ec96d3eeaf8a80c4aa5
cuijianfly <c767332172@163.com>
2021-04-18 14:24:33 +0800

    测试修改提交

---
+++ src/main/java/com/git/demo/Test.java
@@ -0,0 +1,13 @@
+package com.git.demo;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        try {
+            System.out.println(123);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
